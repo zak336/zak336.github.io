9fda30024fd4360b4b81cab740683f02dcc5bea6
Zakee Ahmed <93667785+zak336@users.noreply.github.com>
2023-10-27 22:29:02 +0530

    again

--- docs/1.js
+++ docs/1.js
@@ -1,13 +1,15 @@
 document.querySelector(".cross").style.display = "none";
-document.querySelector(".hamburger").addEventListener("click", () => {
+document.querySelector(".hamburger").addEventListener("click", ()=>{
     document.querySelector(".sidebar").classList.toggle("sidebargo");
-    if (document.querySelector(".sidebar").classList.contains("sidebargo")) {
-        document.querySelector(".sidebar").style.display = "block"; // Corrected this line
-        document.querySelector(".cross").style.display = "none";
-    } else {
-        document.querySelector(".hamburger").style.display = "none";
-        setTimeout(() => {
-            document.querySelector(".cross").style.display = "inline";
-        }, 350);
+    if(document.querySelector(".sidebar").classList.contains("sidebargo")){
+        document.querySelector(".sidebar").style.display="inline";
+        document.querySelector(".cross").style.display="none";
     }
-});
+    else{
+        document.querySelector(".ham").style.display="none";
+        setTimeout(() => { 
+        document.querySelector(".cross").style.display="inline";
+        }, 350)
+    }
+
+})
