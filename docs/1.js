7ee6c6da2f9c17250a9e8a5575e9b33d49e98752
Zakee Ahmed <93667785+zak336@users.noreply.github.com>
2023-10-27 21:10:50 +0530

    correction in sidebar

--- docs/1.js
+++ docs/1.js
@@ -1,15 +1,17 @@
 document.querySelector(".cross").style.display = "none";
-document.querySelector(".hamburger").addEventListener("click", ()=>{
-    document.querySelector(".sidebar").classList.toggle("sidebargo");
-    if(document.querySelector(".sidebar").classList.contains("sidebargo")){
-        document.querySelector(".sidebar").style.display="inline";
-        document.querySelector(".cross").style.display="none";
-    }
-    else{
-        document.querySelector(".ham").style.display="none";
-        setTimeout(() => { 
-        document.querySelector(".cross").style.display="inline";
-        }, 350)
-    }
+document.querySelector(".hamburger").addEventListener("click", () => {
+    const sidebar = document.querySelector(".sidebar");
+    const cross = document.querySelector(".cross");
 
-})
+    sidebar.classList.toggle("sidebargo");
+    
+    if (sidebar.classList.contains("sidebargo")) {
+        sidebar.style.display = "inline";
+        cross.style.display = "none";
+    } else {
+        sidebar.style.display = "none"; // Hide sidebar when it's not active
+        setTimeout(() => {
+            cross.style.display = "inline";
+        }, 350);
+    }
+});
