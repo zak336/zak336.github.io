b55849a333272a6bf7e802be69b2896ef110c594
Zakee Ahmed <93667785+zak336@users.noreply.github.com>
2023-12-08 13:05:10 +0530

    added navigation bar, common styles sheet, commom js sheet

---
+++ docs/java.js
@@ -0,0 +1,14 @@
+// JavaScript to toggle the sidebar on button click
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.sidebar-toggle').addEventListener('click', function () {
+      document.querySelector('.navbar').classList.toggle('show-sidebar');
+    });
+  });
+
+function redirectToGoogleForm() {
+    window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSe0I4oIi6VSQML7vjXyo3SnwJZymzFKR0tMo6QMlff3JkMdMw/viewform?embedded=true', '_blank'; // Replace 'main_page.html' with your main page URL
+  }
+
+function redirectToMainPage() {
+    window.location.href = 'index.html'; // Replace 'main_page.html' with your main page URL
+  }
