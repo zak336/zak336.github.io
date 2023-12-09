32700972f384cfdb66dad53ad76f1a8474482b04
Zakee Ahmed <93667785+zak336@users.noreply.github.com>
2023-12-09 11:38:30 +0530

    boxshadow and java css linking to seperate files

---
+++ docs/main.js
@@ -0,0 +1,34 @@
+// Function to toggle sidebar visibility
+function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('show-sidebar');
+}
+
+// Event listener for the close button to toggle the sidebar
+const closeButton = document.querySelector('.close-sidebar');
+if (closeButton) {
+    closeButton.addEventListener('click', toggleSidebar);
+}
+
+// Function to close sidebar if touched outside the sidebar
+document.addEventListener('click', function (event) {
+    const sidebar = document.querySelector('.sidebar');
+    const sidebarToggle = document.querySelector('.sidebar-toggle');
+
+    // Check if the clicked element is not the sidebar or its toggle button
+    if (!event.target.closest('.sidebar') && event.target !== sidebarToggle) {
+        sidebar.classList.remove('show-sidebar');
+    }
+});
+
+
+
+function redirectToGoogleForm() {
+    window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSe0I4oIi6VSQML7vjXyo3SnwJZymzFKR0tMo6QMlff3JkMdMw/viewform?embedded=true', '_blank'; // Replace 'main_page.html' with your main page URL
+}
+
+
+
+function redirectToMainPage() {
+    window.location.href = 'index.html'; // Replace 'main_page.html' with your main page URL
+}
