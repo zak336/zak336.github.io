ef658bfa0b73737decb512085ddfb0eacaba5045
Zakee Ahmed <93667785+zak336@users.noreply.github.com>
2023-12-09 11:45:10 +0530

    stuff with forms

--- docs/main.js
+++ docs/main.js
@@ -22,10 +22,9 @@ document.addEventListener('click', function (event) {
 });
 
 
-
 function redirectToGoogleForm() {
-    window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSe0I4oIi6VSQML7vjXyo3SnwJZymzFKR0tMo6QMlff3JkMdMw/viewform?embedded=true', '_blank'; // Replace 'main_page.html' with your main page URL
-}
+    window.location.href = 'form.html', '_blank'; // Replace 'main_page.html' with your main page URL
+} //https://docs.google.com/forms/d/e/1FAIpQLSe0I4oIi6VSQML7vjXyo3SnwJZymzFKR0tMo6QMlff3JkMdMw/viewform?embedded=true
 
 
 
