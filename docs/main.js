854b5c5fa6e863877c14c255c80a039c980c7f26
Zakee Ahmed <93667785+zak336@users.noreply.github.com>
2023-12-11 12:13:37 +0530

    adding move to yop button 2

--- docs/main.js
+++ docs/main.js
@@ -66,3 +66,21 @@ window.addEventListener('scroll', function () {
         nav.classList.remove('fixed-nav');
     }
 });
+
+window.onscroll = function () {
+    showButton();
+};
+
+function showButton() {
+    var button = document.getElementById("moveToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+function moveToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
